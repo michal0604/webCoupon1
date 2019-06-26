32b2892e9191c88a3cae064469b2b640e2d1833f
EivyFridman <45360415+EivyFridman@users.noreply.github.com>
2019-06-26 18:13:16 +0300

    commit

--- webCoupon1/src/main/java/com/johnbryce/service/CustomerService.java
+++ webCoupon1/src/main/java/com/johnbryce/service/CustomerService.java
@@ -1,4 +1,5 @@
 package com.johnbryce.service;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,98 +15,116 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
+import com.google.gson.Gson;
 import com.johnbryce.beans.Coupon;
 import com.johnbryce.beans.CouponType;
 import com.johnbryce.exception.CouponException;
+import com.johnbryce.facad.CouponClientFacade;
 import com.johnbryce.facad.CustomerFacad;
+import com.johnbryce.utils.ClientType;
+import com.johnbryce.utils.CouponSystem;
 
 @Path("customer")
 public class CustomerService {
 	@Context
-	private HttpServletRequest request; 
+	private HttpServletRequest request;
 	@Context
 	private HttpServletResponse response;
-	
+
 	private CustomerFacad getFacad() {
-		CustomerFacad customer=(CustomerFacad)request.getSession(false).getAttribute("facade");
+		CustomerFacad customer = (CustomerFacad) request.getSession(false).getAttribute("facade");
 		return customer;
 	}
-	
-	@POST
-	@Path("purchaseCoupon/{coupid}")
+
+	@GET
+	@Path("/login")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String purchaseCoupon(@PathParam ("coupId")long coupId)
-	{
-		CustomerFacad customer= getFacad();
-		Coupon coupon= new Coupon();
+	public String login(@QueryParam("name") String name, @QueryParam("pass") String password) {
+		try {
+			CouponClientFacade facad = CouponSystem.login(name, password, ClientType.CUSTOMER);
+			if (facad != null) {
+				request.getSession(true).setAttribute("facade", facad);
+				return "succesfull login";
+			} else {
+				return "faild login - wrong user or password";
+			}
+		} catch (Exception e) {
+			return "faild login " + e.getMessage();
+		}
+
+	}
+
+	@GET
+	@Path("purchaseCoupon/{coupid}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String purchaseCoupon(@PathParam("coupId") long coupId) {
+		CustomerFacad customer = getFacad();
+		Coupon coupon = new Coupon();
 		try {
 			customer.purchaseCoupon(coupId);
-			
-		}catch (CouponException e) {
-			return "failed to purchase coupon"+e;
+
+		} catch (CouponException e) {
+			return "failed to purchase coupon" + e;
 		}
-		return "coupon purchase"+coupon.getTitle();
+		return new Gson().toJson("coupon purchase" + coupon.getTitle());
 	}
 
-   
-@GET
-@Path("/getAllCouponsByType/{couponType}")
-@Consumes(MediaType.APPLICATION_JSON)
-public Set<Coupon> getAllCouponsByType(@PathParam("couponType") CouponType couponType){
-	Set<Coupon>couponByType=new HashSet<>();
-	CustomerFacad customer=getFacad();
-	try {
-		couponByType=customer.getAllCouponsByType(couponType);
-		
-	} catch (Exception e) {
-		System.out.println(e);
-	}
-	return couponByType;
-	
-}
+	@GET
+	@Path("/getAllCouponsByType/{couponType}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String getAllCouponsByType(@PathParam("couponType") CouponType couponType) {
+		try {
+			Set<Coupon> couponByType = new HashSet<>();
+			CustomerFacad customer = getFacad();
 
-	
-@GET
-@Consumes(MediaType.APPLICATION_JSON)
-@Path("/getAllPurchasedCoupons")
-public Set<Coupon> getAllPurchasedCoupons () {
-	Set<Coupon>allCoupons=new HashSet<>();
-	CustomerFacad customer= getFacad();
-	try {
-		allCoupons=customer.getAllPurchasedCoupons();
-	} catch (Exception e) {
-	System.out.println(e);
-	}
-	return allCoupons;
-}
+			return new Gson().toJson(customer.getAllCouponsByType(couponType));
 
-@GET
-@Consumes(MediaType.APPLICATION_JSON)
-@Path("/getAllPurchasedCouponsByType")
-public Set<Coupon> getAllPurchasedCouponsByType(@PathParam("type")CouponType type) {
-	Set<Coupon>allPurchaseCouponByType=new HashSet<>();
-	CustomerFacad customer=getFacad();
-	try {
-		allPurchaseCouponByType=customer.getAllCouponsByType(type);
-	} catch (Exception e) {
-		System.out.println(e);	
-	}
-	return allPurchaseCouponByType;
-}
+		} catch (Exception e) {
+			return "faild "+e.getMessage();
+		}
 
-@GET
-@Consumes(MediaType.APPLICATION_JSON)
-@Path("/getAllPurchasedCouponsByPrice")
-public Set<Coupon> getAllPurchasedCouponsByPrice(@PathParam("price")long price) {
-	CustomerFacad customer=getFacad();
-	Set<Coupon>allPurchasedCouponsByPrice=new HashSet<>();
-	try {
-		allPurchasedCouponsByPrice=customer.getAllPurchasedCouponsByPrice(price);
-	} catch (Exception e) {
-		System.out.println(e);
-		
 	}
-	return allPurchasedCouponsByPrice;
-}
-}
 
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/getAllPurchasedCoupons")
+	public Set<Coupon> getAllPurchasedCoupons() {
+		Set<Coupon> allCoupons = new HashSet<>();
+		CustomerFacad customer = getFacad();
+		try {
+			allCoupons = customer.getAllPurchasedCoupons();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return allCoupons;
+	}
+
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/getAllPurchasedCouponsByType")
+	public Set<Coupon> getAllPurchasedCouponsByType(@PathParam("type") CouponType type) {
+		Set<Coupon> allPurchaseCouponByType = new HashSet<>();
+		CustomerFacad customer = getFacad();
+		try {
+			allPurchaseCouponByType = customer.getAllCouponsByType(type);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return allPurchaseCouponByType;
+	}
+
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("/getAllPurchasedCouponsByPrice")
+	public Set<Coupon> getAllPurchasedCouponsByPrice(@PathParam("price") long price) {
+		CustomerFacad customer = getFacad();
+		Set<Coupon> allPurchasedCouponsByPrice = new HashSet<>();
+		try {
+			allPurchasedCouponsByPrice = customer.getAllPurchasedCouponsByPrice(price);
+		} catch (Exception e) {
+			System.out.println(e);
+
+		}
+		return allPurchasedCouponsByPrice;
+	}
+}
